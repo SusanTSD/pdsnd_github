cef99809466b7b15bfc86dab1feabced4d3a0d9d
Tsai-sang Dederichs <TSLO1004@gmail.com>
2023-04-11 07:52:42 +0200

    refactor: add Day_period list

--- bikeshare.py
+++ bikeshare.py
@@ -5,26 +5,26 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv'}
-              
+
 def get_filters():
     """Asks user to specify a city, month, and day to analyze. Returns:(str) city - name of the city to analyze (str) month - name of the month to filter by, or "all" to apply no month filter (str) day - name of the day of week to filter by, or "all" to apply no day filter"""
     print('Hello! Let\'s explore some US bikeshare data!')
     cities = ['chicago','new york city','washington']
     months = ['january','february','march','april','may','june','all']
     days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday','all']
-
+	Day_period = ['morning','afternoon','evening']
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = input("Which city would you like to explore: Chicago, New York city, Washington:\n").lower()
-    
+
     while (city not in cities):
         city = input("Please type your answer again: Chicago, New York city or Washington, or all:").lower()
-    
+
     # TO DO: get user input for month (all, january, february, ... , june)
     month = input("Which month would you like to explore? (Januray, Februrary, March, April, May, June, or all)\n").lower()
-    
+
     while (month not in months):
         month = input("Please type your answer again:\n").lower()
-  
+
     # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
     day = input("Which month would you like to explore? (Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, or all)\n").lower()
     while (day not in days):
@@ -32,17 +32,17 @@ def get_filters():
     else:
         print('-'*50)
         print(city, month, day)
-    
+
     return city, month, day
 
 def load_data(city, month, day):
     """Loads data for the specified city and filters by month and day if applicable. Args:(str) city - name of the city to analyze(str) month - name of the month to filter by, or "all" to apply no month filter (str) day - name of the day of week to filter by, or "all" to apply no day filter Returns: df - Pandas DataFrame containing city data filtered by month and day"""
     # load data file into a dataframe
     df = pd.read_csv(CITY_DATA[city])
-    
+
     # convert the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
-    
+
     # extract month and day of week from Start Time to create new columns
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday
@@ -52,8 +52,8 @@ def load_data(city, month, day):
         # use the index of the months list to get the corresponding int
         months = ['january','february','march','april','may','june']
         month = months.index(month) + 1
-      
-    
+
+
         # filter by month to create the new dataframe
         df = df[df['month'] == month]
       # filter by day of week if applicable
@@ -61,13 +61,13 @@ def load_data(city, month, day):
         # filter by day of week to create the new dataframe
         day_num = {'monday':0,'tuesday':1,'wednesday':2,'thursday':3,'friday':4,'saturday':5,'sunday':6}
         df = df[df['day_of_week'] == day_num[day]]
-    
+
     return df
 
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
     start_time = time.time()
-    
+
     print('\nCalculating The Most Frequent Times of Travel...\n')
     # display the most common month
     month_number = {1:'January', 2:"February", 3:'March', 4:'April', 5:'May', 6:'June'}
@@ -104,7 +104,7 @@ def station_stats(df):
     df['start_end_route'] = df['Start Station'] + " to " +df['End Station']
     popular_route = df['start_end_route'].value_counts()
     print('The most popular route is {}.'.format(popular_route.idxmax()))
-  
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-' * 40)
 
@@ -153,8 +153,8 @@ def user_stats(df):
       print("People born in {} use our service most often.".format(int(num_birth_year.idxmax())))
     else:
       print("No birth year information recorded.")
-    
-    
+
+
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -165,7 +165,7 @@ def display_raw_data(df):
     raw = input("Would you like to see the raw data? 'yes' or 'no'\n").lower() # TO DO: convert the user input to lower case using lower() function
     pd.set_option('display.max_columns',200)
 
-    while True:            
+    while True:
         if raw == 'no':
             break
         elif raw == 'yes':
